8d7517c58c86ee59a655ea2e8b4bb6b4e19846e7
hsahu26 <harishcms@gmail.com>
2021-07-09 16:01:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
  </head>
  <body>
  <?php
- $ipaddress = getenv("REMOTE_ADDR") ;
+ $ns = getenv("MY_POD_NAMESPACE") ;
+ $podname = getenv("MY_POD_NAME") ;
+ $ipaddress = getenv("REMOTE_ADDR") ; 
+ Echo "Your IP Address is " . $ns;
+ Echo "Your IP Address is " . $podname;
  Echo "Your IP Address is " . $ipaddress;
-?>
+ ?>
  </body>
 </html>
